c1025e5178abe70d081ff757cfc43b1db1f0c87e
balezz <33553428+balezz@users.noreply.github.com>
2019-03-14 20:50:09 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package ru.metrolog;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] testArray1 = {5, 2, 3, 4, 5};
+        int[] testArray2 = {555, 212, 34, 4444, 05};
+
+        Task1A task1A = new Task1A();
+//        task1A.greetingUser();
+//        task1A.reverseArgs(args);
+//        task1A.randomNumbers();
+//        task1A.passwordCompare();
+//        task1A.sumProdArgs(args);
+//        task1A.authorDate();
+
+        Task1B task1B = new Task1B(testArray2);
+//        task1B.enterNumbers();
+//        task1B.printEvenOdds();
+//        task1B.printMinMax();
+//        task1B.dividedOn3and9();
+//        task1B.dividedOn5and7();
+//        task1B.sortBubble();
+        task1B.noSame3digits();
+
+    }
+}
---
+++ Task1B.java
@@ -0,0 +1,144 @@
+package ru.metrolog;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Solutions for chapter 1 in Java Programming Methods
+ * by BlinovIN and RomanchicVS, 2013
+ */
+public class Task1B {
+
+    /* Count and array for input numbers */
+    int N;
+    int[] array;
+
+    /* Constructor only for N,
+     * numbers input from console */
+    public Task1B(int n) {
+        N = n;
+        array = new int[N];
+    }
+
+    /* Constructor for N and array,
+     * no need to input from console */
+    public Task1B(int[] array) {
+        N = array.length;
+        this.array = array;
+    }
+
+    /* Prepare operation */
+    public void enterNumbers() {
+        System.out.println("Enter " + N + " integer numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+    }
+
+    /* Task 1B-1*/
+    public void printEvenOdds() {
+        System.out.println("Even: \t ");
+        for (int i = 0; i < N; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+        System.out.println("Odds: \t ");
+        for (int i = 0; i < N; i++) {
+            if (array[i] % 2 == 1) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+    /* Task 1B-2 */
+    public void printMinMax() {
+        int max_num = array[0];
+        int min_num = array[0];
+        for (int i = 1; i < N; i++) {
+            if (array[i] > max_num) {
+                max_num = array[i];
+            }
+            if (array[i] < min_num) {
+                min_num = array[i];
+            }
+        }
+        System.out.println();
+        System.out.print("Maximum number is: " + max_num);
+        System.out.println();
+        System.out.print("Minimum number is: " + min_num);
+    }
+
+
+    /* Task 1B-3 */
+    public void dividedOn3and9() {
+        System.out.print("Divided on 3: ");
+        for (int n : array) {
+            if (n % 3 == 0) {
+                System.out.print(n + "; ");
+            }
+        }
+        System.out.println();
+        System.out.print("Divided on 9: ");
+        for (int n : array) {
+            if (n % 9 == 0) {
+                System.out.print(n + "; ");
+            }
+        }
+        System.out.println();
+    }
+
+    /* Task 1B-4 */
+    public void dividedOn5and7() {
+        System.out.print("Divided on 5: ");
+        for (int n : array) {
+            if (n % 5 == 0) {
+                System.out.print(n + "; ");
+            }
+        }
+        System.out.println();
+        System.out.print("Divided on 7: ");
+        for (int n : array) {
+            if (n % 7 == 0) {
+                System.out.print(n + "; ");
+            }
+        }
+        System.out.println();
+    }
+
+    /* Task 1B-5 */
+    public void sortBubble() {
+        int bubble = 0;
+        for (int j = 1; j < N - 1; j++) {
+            for (int i = 0; i < N - j; i++) {
+                if (array[i] > array[i + 1]) {
+                    bubble = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = bubble;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+
+    /* Task 1B-6 */
+    public void noSame3digits() {
+        int d1, d10, d100 = 0;
+        System.out.println("Numbers with different 3 digits");
+        for (int n:array) {
+            if ((n > 99) && (n < 1000)) {
+                d1 = n%10;
+                d10 = (n/10)%10;
+                d100 = (n/100)%10;
+                if ((d1 != d10) || (d1 != d100)) {
+                    System.out.print(n + "; ");
+                }
+            }
+        }
+
+    }
+}
